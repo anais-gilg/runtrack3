29363cdf4df879f98ddbbeb1856b54dc6c68bb81
anais-gilg <anais.gilg@laplateforme.io>
2023-01-16 17:08:19 +0100

    afficher les jours de la semaine

---
+++ Jour1/job05/script.js
@@ -0,0 +1,8 @@
+function afficherjourssemaines(){
+    let jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    for(let i=0 ; jourssemaines[i] ; i++){
+        console.log(jourssemaines[i]);
+    }
+}
+
+afficherjourssemaines();
